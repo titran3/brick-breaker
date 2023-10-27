cf08da1676afadb16b024806479af193a87237bf
angie <88114622+angiefn@users.noreply.github.com>
2023-10-27 13:25:14 -0700

    part 2

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 class_name Ball
 
+signal life_lost
+
 const VELOCITY_LIMIT = 40
 
 @export var ball_speed = 20
 @export var lifes = 3
+@export var death_zone: DeathZone
+@export var ui: UI
 
 var speed_up_factor = 1.05
 var start_position: Vector2
@@ -13,7 +17,9 @@ var start_position: Vector2
 @onready var collision_shape_2d = $CollisionShape2D
 
 func _ready():
+	ui.set_lifes(lifes)
 	start_position = position
+	death_zone.life_lost.connect(on_life_lost)
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * ball_speed * delta)
@@ -27,4 +33,17 @@ func start_ball():
 	randomize()
 	
 	velocity = Vector2(randf_range(-1, 1), randf_range(-.1, -1)).normalized() * ball_speed
+	
+func on_life_lost():
+	lifes -= 1
+	if lifes == 0:
+		ui.game_over()
+	else:
+		life_lost.emit()
+		reset_ball()
+		ui.set_lifes(lifes)
+		
+func reset_ball():
+	position = start_position
+	velocity = Vector2.ZERO
 	
